fd1038efec54efa0a3815548395d983b40c59b57
TetianaYasynovska <150725014+TatiYas@users.noreply.github.com>
2025-12-29 20:21:59 +0100

    Update page.tsx

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,26 +1,73 @@
-import { fetchNoteById } from "@/lib/api";
 import { QueryClient, HydrationBoundary, dehydrate } from "@tanstack/react-query";
+import { fetchNoteById } from "@/lib/api";
 import NoteDetailsClient from "./NoteDetails.client";
+import type { Metadata } from "next";
 
 type NoteDetailsPageProps = {
-  params: Promise<{ id: string }>; }
+  params: { id: string };
+};
+
+export async function generateMetadata(
+  { params }: NoteDetailsPageProps
+): Promise<Metadata> {
+  const note = await fetchNoteById(params.id);
+
+  return {
+    title: note.title,
+    description: note.content.slice(0, 150),
+    openGraph: {
+      title: note.title,
+      description: note.content.slice(0, 150),
+      type: "article",
+    },
+  };
+}
+
 
 export default async function NoteDetailsPage({ params }: NoteDetailsPageProps) {
-
-  const { id } = await params;
+  const { id } = params;
 
   const queryClient = new QueryClient();
 
-     await queryClient.prefetchQuery({
-    queryKey: ["note", id],  
+  await queryClient.prefetchQuery({
+    queryKey: ["note", id],
     queryFn: () => fetchNoteById(id),
   });
 
   return (
-    <div>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-       <NoteDetailsClient />
-      </HydrationBoundary>
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NoteDetailsClient id={id} />
+    </HydrationBoundary>
   );
 }
+
+
+
+
+
+//import { fetchNoteById } from "@/lib/api";
+//import { QueryClient, HydrationBoundary, dehydrate } from "@tanstack/react-query";
+//import NoteDetailsClient from "./NoteDetails.client";
+
+//type NoteDetailsPageProps = {
+ // params: Promise<{ id: string }>; }
+
+//export default async function NoteDetailsPage({ params }: NoteDetailsPageProps) {
+
+  //const { id } = await params;
+
+ // const queryClient = new QueryClient();
+
+    // await queryClient.prefetchQuery({
+    //queryKey: ["note", id],  
+    //queryFn: () => fetchNoteById(id),
+  //});
+
+ // return (
+    //<div>
+     // <HydrationBoundary state={dehydrate(queryClient)}>
+      // <NoteDetailsClient />
+     // </HydrationBoundary>
+    //</div>
+ // );
+//}//
