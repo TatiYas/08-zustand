6b89a076ccf9249a114623d04d35bebcddbbe240
TetianaYasynovska <150725014+TatiYas@users.noreply.github.com>
2025-12-29 22:03:35 +0100

    Update NoteDetails.client.tsx

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -1,33 +1,49 @@
-"use client";
-import css from './NoteDetails.client.module.css'
-import { useParams } from 'next/navigation';
+'use client';
+
+import css from './NoteDetails.module.css';
 import { useQuery } from '@tanstack/react-query';
-import {fetchNoteById} from "../../../lib/api" ;
-import type { Note } from "../../../types/note"; 
+import { fetchNoteById } from '@/lib/api';
+import type { Note } from '@/types/note';
 
+type Props = {
+  id: string;
+};
 
-export default function NoteDetails (){ 
-   
-const {id}= useParams<{id:string}>(); 
+export default function NoteDetailsClient({ id }: Props) {
+  const { data, isLoading, isError } = useQuery<Note>({
+    queryKey: ['note', id],
+    queryFn: () => fetchNoteById(id),
+    refetchOnMount: false,
+  });
 
-const {data: note, isLoading, isError} =useQuery<Note, Error>({
-	queryKey:["note", id],
-	queryFn: ()=> fetchNoteById(id),
-	refetchOnMount: false,
-});
+  if (isLoading) {
+    return <p className={css.text}>Loading, please wait...</p>;
+  }
 
-if (isLoading) return <p className={css.text}>Loading, please wait...</p>
-if (isError|| !note) return <p className={css.text}> Something went wrong. Could not upload details</p>;
+  if (isError || !data) {
+    return (
+      <p className={css.text}>
+        Something went wrong. Could not load note details.
+      </p>
+    );
+  }
 
-return(
-<div className={css.container}>
-	<div className={css.item}>
-	  <div className={css.header}>
-	    <h2>{note.title}</h2>
-	  </div>
-	  <p className={css.content}>{note.content}</p>
-	  <p className={css.date}>Created date: {new Date(note.createdAt).toLocaleDateString()}</p>
-	</div>
-</div>)
+  return (
+    <div className={css.container}>
+      <div className={css.item}>
+        <div className={css.header}>
+          <h2>{data.title}</h2>
+        </div>
+
+        <p className={css.content}>{data.content}</p>
+
+        <p className={css.date}>
+          Created date:{' '}
+          {new Date(data.createdAt).toLocaleDateString()}
+        </p>
+
+        <p className={css.tag}>{data.tag}</p>
+      </div>
+    </div>
+  );
 }
- 
