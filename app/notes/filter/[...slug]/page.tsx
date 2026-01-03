e539359ba19a203187f196c589123070dba0593d
TatiYas <iasinovska_lawyer@ukr.net>
2026-01-03 12:55:28 +0100

    Update page.tsx

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,15 +1,18 @@
 import { HydrationBoundary, dehydrate, QueryClient } from "@tanstack/react-query";
-import NotesClient from "@/app/notes/filter/[...slug]/Notes.client"; 
+import NotesClient from "./Notes.client"; 
 import { fetchNotes } from "@/lib/api";
 import { Metadata } from "next";
 
 type Props = {
- params: { slug?: string[] };
+ params: Promise<{ slug?: string[] }>;
 };
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
- const slug = params.slug ?? [];
- const filter = slug.length > 0 ? slug.join("/") : "all";
+ 
+ const { slug } = await params;
+ const resolvedSlug = slug ?? [];
+
+ const filter = resolvedSlug.length > 0 ? resolvedSlug.join("/") : "all";
 
  const title = `Notes filter: ${filter} | NoteHub`;
  const description = `Notes filtered by ${filter} in NoteHub`;
@@ -20,32 +23,25 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
  openGraph: {
  title,
  description,
- url: `https://08-zustand-one-plum.vercel.app//notes/filter/${filter}`,
- siteName: "NoteHub",
- images: [
- {
- url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
- width: 1200,
- height: 630,
- alt: "NoteHub",
- },
- ],
- locale: "en_US",
- type: "website",
+ 
  },
  };
 }
 
+
 export default async function Page({ params }: Props) {
+
+ const { slug } = await params;
+ const resolvedSlug = slug ?? [];
+
  const queryClient = new QueryClient();
- const slug = params.slug ?? [];
 
- const tag = slug[0] === "All" ? undefined : slug[0];
 
+ const tag = resolvedSlug[0] === "All" ? undefined : resolvedSlug[0];
 
  await queryClient.prefetchQuery({
  queryKey: ["notes", { query: "", page: 1, tag: tag ?? null }],
- queryFn: () => fetchNotes(1, "", tag), 
+ queryFn: () => fetchNotes(1, "", tag ?? undefined),
  });
 
  return (
@@ -54,3 +50,4 @@ export default async function Page({ params }: Props) {
  </HydrationBoundary>
  );
 }
+
